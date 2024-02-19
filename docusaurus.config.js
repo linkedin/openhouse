9aa209275b2a851fddde5e176d7b023a977067fe
Sushant Raikar <sraikar@linkedin.com>
2024-02-18 21:24:57 -0800

    Documentation Base URL fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/openhouse/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
