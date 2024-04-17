df3e22af8ecff0e64290de82bc84422dca726a39
Sushant Raikar <sraikar@linkedin.com>
2024-04-17 14:37:04 -0700

    Add cluster.yaml

---
+++ cluster/storage/src/main/java/com/linkedin/openhouse/cluster/storage/configs/StorageProperties.java
@@ -0,0 +1,35 @@
+package com.linkedin.openhouse.cluster.storage.configs;
+
+import com.linkedin.openhouse.cluster.configs.YamlPropertySourceFactory;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "cluster.storages")
+@PropertySource(
+    name = "clusterStorage",
+    value = "file:${OPENHOUSE_CLUSTER_CONFIG_PATH:/var/config/cluster.yaml}",
+    factory = YamlPropertySourceFactory.class,
+    ignoreResourceNotFound = true)
+@Getter
+@Setter
+public class StorageProperties {
+  private String defaultType;
+  private Map<String, StorageTypeProperties> types = new HashMap<>();
+
+  @Getter
+  @Setter
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Builder(toBuilder = true)
+  public static class StorageTypeProperties {
+    private String scheme;
+    private String rootPath;
+    private String endpoint;
+    @Builder.Default private Map<String, String> parameters = new HashMap<>();
+  }
+}
---
+++ services/tables/src/test/java/com/linkedin/openhouse/tables/mock/storage/StoragePropertiesConfigTest.java
@@ -0,0 +1,53 @@
+package com.linkedin.openhouse.tables.mock.storage;
+
+import com.linkedin.openhouse.cluster.storage.configs.StorageProperties;
+import com.linkedin.openhouse.tables.mock.properties.CustomClusterPropertiesInitializer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(initializers = CustomClusterPropertiesInitializer.class)
+public class StoragePropertiesConfigTest {
+  @Autowired private StorageProperties storageProperties;
+
+  private static final String DEFAULT_TYPE = "hdfs";
+  private static final String ANOTHER_TYPE = "s3";
+  private static final String NON_EXISTING_TYPE = "non-existing-type";
+
+  @Test
+  public void testDefaultType() {
+    Assertions.assertEquals(DEFAULT_TYPE, storageProperties.getDefaultType());
+  }
+
+  @Test
+  public void testStorageTypeScheme() {
+    Assertions.assertEquals(
+        DEFAULT_TYPE, storageProperties.getTypes().get(DEFAULT_TYPE).getScheme());
+  }
+
+  @Test
+  public void testStorageTypeLookup() {
+    Assertions.assertEquals(
+        ANOTHER_TYPE, storageProperties.getTypes().get(ANOTHER_TYPE).getScheme());
+  }
+
+  @Test
+  public void testStorageTypeVariableProperties() {
+    Assertions.assertFalse(
+        storageProperties.getTypes().get(DEFAULT_TYPE).getParameters().isEmpty());
+  }
+
+  @Test
+  public void testUnsetPropertiesAreNull() {
+    Assertions.assertNull(storageProperties.getTypes().get(NON_EXISTING_TYPE));
+  }
+
+  @AfterAll
+  static void unsetSysProp() {
+    System.clearProperty("OPENHOUSE_CLUSTER_CONFIG_PATH");
+  }
+}
