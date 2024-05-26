d521426e4217281971dc8a6e16e1cc96f62ffcaf
Stas Pak <stpak@linkedin.com>
2024-05-26 09:35:41 -0700

    Add DLO strategy spec and persistence

---
+++ libs/datalayout/src/main/java/com/linkedin/openhouse/datalayout/config/DataCompactionConfig.java
@@ -0,0 +1,44 @@
+package com.linkedin.openhouse.datalayout.config;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.apache.iceberg.TableProperties;
+import org.apache.iceberg.actions.BinPackStrategy;
+import org.apache.iceberg.actions.RewriteDataFiles;
+
+@ToString
+@Builder
+@Getter
+@EqualsAndHashCode
+public final class DataCompactionConfig {
+  public static final long TARGET_BYTE_SIZE_DEFAULT =
+      TableProperties.WRITE_TARGET_FILE_SIZE_BYTES_DEFAULT;
+  public static final double MIN_BYTE_SIZE_RATIO_DEFAULT =
+      BinPackStrategy.MIN_FILE_SIZE_DEFAULT_RATIO;
+  public static final double MAX_BYTE_SIZE_RATIO_DEFAULT =
+      BinPackStrategy.MAX_FILE_SIZE_DEFAULT_RATIO;
+  public static final int MIN_INPUT_FILES_DEFAULT = BinPackStrategy.MIN_INPUT_FILES_DEFAULT;
+  public static final int MAX_CONCURRENT_FILE_GROUP_REWRITES_DEFAULT =
+      RewriteDataFiles.MAX_CONCURRENT_FILE_GROUP_REWRITES_DEFAULT;
+  public static final int PARTIAL_PROGRESS_MAX_COMMITS_DEFAULT =
+      RewriteDataFiles.PARTIAL_PROGRESS_MAX_COMMITS_DEFAULT;
+  public static final long MAX_FILE_GROUP_SIZE_BYTES_DEFAULT =
+      RewriteDataFiles.MAX_FILE_GROUP_SIZE_BYTES_DEFAULT;
+  public static final double ENTROPY_THRESHOLD_DEFAULT =
+      156E12; // >= 1% files below 75% default target size
+
+  @Builder.Default private long targetByteSize = TARGET_BYTE_SIZE_DEFAULT;
+  @Builder.Default private double minByteSizeRatio = MIN_BYTE_SIZE_RATIO_DEFAULT;
+  @Builder.Default private double maxByteSizeRatio = MAX_BYTE_SIZE_RATIO_DEFAULT;
+  @Builder.Default private int minInputFiles = MIN_INPUT_FILES_DEFAULT;
+
+  @Builder.Default
+  private int maxConcurrentFileGroupRewrites = MAX_CONCURRENT_FILE_GROUP_REWRITES_DEFAULT;
+
+  @Builder.Default private boolean partialProgressEnabled = true;
+  @Builder.Default private int partialProgressMaxCommits = PARTIAL_PROGRESS_MAX_COMMITS_DEFAULT;
+  @Builder.Default private long maxFileGroupSizeBytes = MAX_FILE_GROUP_SIZE_BYTES_DEFAULT;
+  @Builder.Default private double fileEntropyThreshold = ENTROPY_THRESHOLD_DEFAULT;
+}
