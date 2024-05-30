42649b1e07a60c9c577e42df3525c9d590831f91
Lavina Jain <lajain@linkedin.com>
2024-05-30 12:09:06 -0700

    Add comments for javadocs

--- cluster/storage/src/main/java/com/linkedin/openhouse/cluster/storage/s3/S3StorageClient.java
+++ cluster/storage/src/main/java/com/linkedin/openhouse/cluster/storage/s3/S3StorageClient.java
@@ -16,16 +16,28 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
 
+/**
+ * S3StorageClient is an implementation of the StorageClient interface for S3. It uses the {@link
+ * S3Client} to interact with S3 storage. An AWS_REGION must be set either in the environment
+ * variable or in the storage properties for the client initialization.
+ */
 @Slf4j
 @Lazy
 @Component
 public class S3StorageClient implements StorageClient<S3Client> {
+  // Storage type.
   private static final StorageType.Type S3_TYPE = StorageType.S3;
 
+  // Storage properties.
   @Autowired private StorageProperties storageProperties;
 
+  // S3 client.
   private S3Client s3;
 
+  /**
+   * Initialize the S3 client when the bean is accessed the first time. AWS_REGION must be set in
+   * the evironment variable or in the storage properties.
+   */
   @PostConstruct
   public synchronized void init() {
     // TODO: Validate properties.
