baf0c0adee3b70459bd037e6fd798b6c601c4397
Stas Pak <stpak@linkedin.com>
2024-06-03 17:48:50 -0700

    Fix & refactor

--- services/jobs/src/main/java/com/linkedin/openhouse/jobs/api/validator/impl/OpenHouseJobsApiValidator.java
+++ services/jobs/src/main/java/com/linkedin/openhouse/jobs/api/validator/impl/OpenHouseJobsApiValidator.java
@@ -6,12 +6,14 @@ import com.linkedin.openhouse.common.api.validator.ApiValidatorUtil;
 import com.linkedin.openhouse.common.exception.RequestValidationFailureException;
 import com.linkedin.openhouse.jobs.api.spec.request.CreateJobRequestBody;
 import com.linkedin.openhouse.jobs.api.validator.JobsApiValidator;
+import com.linkedin.openhouse.jobs.model.JobConf;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import lombok.NonNull;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -41,14 +43,7 @@ public class OpenHouseJobsApiValidator implements JobsApiValidator {
               "clusterId : provided %s, %s",
               createJobRequestBody.getClusterId(), ALPHA_NUM_UNDERSCORE_ERROR_MSG_HYPHEN_ALLOW));
     }
-    final Map<String, String> executionConf = createJobRequestBody.getJobConf().getExecutionConf();
-    if (!MapUtils.isEmpty(executionConf)
-        && !JobConfValidator.executionConfigValidator(executionConf)) {
-      validationFailures.add(
-          String.format(
-              "memory : provided %s, %s",
-              executionConf.get(JOB_MEMORY_CONFIG), SPARK_MEMORY_FORMAT));
-    }
+    JobConfValidator.validate(createJobRequestBody.getJobConf(), validationFailures);
     if (!validationFailures.isEmpty()) {
       throw new RequestValidationFailureException(validationFailures);
     }
@@ -56,9 +51,20 @@ public class OpenHouseJobsApiValidator implements JobsApiValidator {
 
   // inner class to validate jobConf fields.
   private static class JobConfValidator {
-    public static boolean executionConfigValidator(Map<String, String> executionConf) {
-      String memoryConfig = executionConf.getOrDefault(JOB_MEMORY_CONFIG, "");
-      return memoryConfig.matches(SPARK_MEMORY_FORMAT);
+    private static boolean validateExecutionConfig(Map<String, String> conf) {
+      String memory = conf.getOrDefault(JOB_MEMORY_CONFIG, "");
+      return memory.matches(SPARK_MEMORY_FORMAT);
+    }
+
+    private static void validate(@NonNull JobConf conf, List<String> validationFailures) {
+      final Map<String, String> executionConfig = conf.getExecutionConf();
+      if (MapUtils.isNotEmpty(executionConfig)
+          && !JobConfValidator.validateExecutionConfig(executionConfig)) {
+        validationFailures.add(
+            String.format(
+                "memory : provided %s, %s",
+                executionConfig.get(JOB_MEMORY_CONFIG), SPARK_MEMORY_FORMAT));
+      }
     }
   }
 }
