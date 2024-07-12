7b615037f76e317760bcc8e758df03071a9cfce0
kmcclenn <kmcclenn@mit.edu>
2024-07-12 14:02:05 -0700

    added k8s variables

--- infra/recipes/terraform/azure/variables.tf
+++ infra/recipes/terraform/azure/variables.tf
@@ -1,19 +1,3 @@
-variable "k8s_client_certificate" {
-  type = string
-  description = "The client certificate for AKS."
-}
-
-variable "k8s_client_key" {
-  type = string
-  description = "The client key for AKS."
-  sensitive = true
-}
-
-variable "k8s_cluster_ca_certificate" {
-  type = string
-  description = "The cluster certificate for AKS."
-}
-
 variable "resource_group_name" {
     type = string
     default = "resources"
