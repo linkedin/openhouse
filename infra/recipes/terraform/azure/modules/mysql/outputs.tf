b04bb5c3c36366700922fccc940dab43dd79f7c7
Kai McClennen <63942422+kmcclenn@users.noreply.github.com>
2024-07-16 19:01:48 -0700

    [1/5] Set up basic Azure sandbox environment, providers, etc. (#136)

---
+++ infra/recipes/terraform/azure/modules/mysql/outputs.tf
@@ -0,0 +1,3 @@
+output "mysql_server_name" {
+  value = azurerm_mysql_flexible_server.mysql_server.name
+}
---
+++ infra/recipes/terraform/azure/modules/mysql/variables.tf
@@ -0,0 +1,31 @@
+variable "subnet_id" {
+    type = string
+    description = "The id of the MySQL subnet."
+}
+
+variable "server_name" {
+    type = string
+    description = "The name for the MySQL server."
+}
+
+variable "db_admin_login" {
+    type = string
+    description = "The username for the MySQL server admin."
+}
+
+variable "db_admin_password" {
+    type = string
+    description = "The password for the MySQL server admin."
+    sensitive = true
+}
+
+variable "db_name" {
+    type = string
+    description = "The name for the MySQL database."
+}
+
+variable "server_sku" {
+    type = string
+    description = "The SKU for the MySQL flexible server."
+    default = "GP_Standard_D2ds_v4"
+}
