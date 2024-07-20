371a52df6c435f85604553d443015110d848354d
kmcclenn <kmcclenn@mit.edu>
2024-07-19 17:10:03 -0700

    tables pod now runs

--- infra/recipes/terraform/azure/modules/helm_release/main.tf
+++ infra/recipes/terraform/azure/modules/helm_release/main.tf
@@ -1,30 +1,30 @@
-# resource "helm_release" "tables" {
-#   chart = "tables"
-#   name = "openhouse-tables-service"
-#   repository = "../../../../k8s/helm"
+resource "helm_release" "tables" {
+  chart = "tables"
+  name = "openhouse-tables-service"
+  repository = "../../../../k8s/helm"
 
-#   values = ["${file("../../../../k8s/environments/azure/sandbox/tables/values.yaml")}"]
+  values = ["${file("../../../../k8s/environments/azure/sandbox/tables/values.yaml")}"]
 
-#   set {
-#     name = "storageProperties.containerName"
-#     value = var.container_name
-#   }
+  set {
+    name = "storageProperties.containerName"
+    value = var.container_name
+  }
 
-#   set {
-#     name = "storageProperties.storageAccountName"
-#     value = var.storage_account_name
-#   }
+  set {
+    name = "storageProperties.storageAccountName"
+    value = var.storage_account_name
+  }
 
-#   set {
-#     name = "storageProperties.storageAccountKey"
-#     value = var.storage_account_key
-#   }
+  set {
+    name = "storageProperties.storageAccountKey"
+    value = var.storage_account_key
+  }
 
-#   set {
-#     name = "housetables.database.url"
-#     value = "mysql://${var.db_username}:${var.db_password}@${var.server_name}.mysql.database.azure.com:8080/${var.db_name}"
-#   }
-# }
+  set_sensitive {
+    name = "housetables.database.url"
+    value = "mysql://${var.db_username}:${var.db_password}@${var.server_name}.mysql.database.azure.com:8080/${var.db_name}"
+  }
+}
 
 resource "helm_release" "housetables" {
   chart = "housetables"
--- infra/recipes/terraform/azure/modules/image/main.tf
+++ infra/recipes/terraform/azure/modules/image/main.tf
@@ -7,8 +7,23 @@ resource "docker_image" "housetables" {
     }
 }
 
-resource "docker_registry_image" "registry" {
+resource "docker_registry_image" "hts_registry" {
     name = "${docker_image.housetables.name}"
 
     depends_on = [ docker_image.housetables ]
+}
+
+resource "docker_image" "tables" {
+    name = "${var.registry_login_server}/openhouse-tables-service:latest"
+
+    build {
+        context = "${path.cwd}/../../../../../.."
+        dockerfile = "tables-service.Dockerfile"
+    }
+}
+
+resource "docker_registry_image" "tables_registry" {
+    name = "${docker_image.tables.name}"
+
+    depends_on = [ docker_image.tables ]
 }
