3ffb26b948c67e7f27a65818584c759df406a422
Stas Pak <stpak@linkedin.com>
2024-08-01 12:59:03 -0700

    Add docstring to partition stat class

--- libs/datalayout/src/main/java/com/linkedin/openhouse/datalayout/datasource/PartitionStat.java
+++ libs/datalayout/src/main/java/com/linkedin/openhouse/datalayout/datasource/PartitionStat.java
@@ -6,12 +6,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/** Represents table partition. */
+/** Represents table partition stats. */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PartitionStat {
+  // list of transformed values for a given table partition
+  // e.g. if table is partitioned by (datepartition: string, state: string)
+  // the list could be ["2024-01-01", "CA"]
   private List<String> values;
   private int fileCount;
 }
