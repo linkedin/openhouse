4de9184ef68275c51b6509779b1ebbb9fce49cb7
Kai McClennen <63942422+kmcclenn@users.noreply.github.com>
2024-08-05 10:36:47 -0700

    Client Cluster for Azure Sandbox (#153)
    ## Summary
    
    Added support for client cluster to make requests to openhouse services
    hosted in Azure.
    
    ## Changes
    
    - [ ] Client-facing API Changes
    - [ ] Internal API Changes
    - [ ] Bug Fixes
    - [x] New Features
    - [ ] Performance Improvements
    - [ ] Code Style
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Tests
    
    Allowed connected to local spark container to make requests to Openhouse
    tables service deployed with AKS.
    
    ## Testing Done
    
    - [x] Manually Tested on local docker setup. Please include commands
    ran, and their output.
    - [ ] Added new tests for the changes made.
    - [ ] Updated existing tests to reflect the changes made.
    - [ ] No tests added or updated. Please explain why. If unsure, please
    feel free to ask for help.
    - [ ] Some other form of testing like staging or soak time in
    production. Please explain.
    
    See video:
    
    
    https://github.com/user-attachments/assets/7c1c0460-1836-4da0-a57d-577b66465294
    
    # Additional Information
    
    - [ ] Breaking Changes
    - [ ] Deprecations
    - [ ] Large PR broken into smaller PRs, and PR plan linked in the
    description.
    
    For all the boxes checked, include additional details of the changes
    made in this pull request.
    
    ---------
    
    Co-authored-by: Abhishek Nath <anath1@linkedin.com>

---
+++ infra/recipes/terraform/azure/environments/sandbox/spark-client.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ACCOUNT_KEY=$(terraform output -raw storage_account_key)
+ACCOUNT_NAME=$(terraform output -raw storage_account_name)
+RESOURCE_GROUP=$(terraform output -raw resource_group_name)
+CLUSTER_NAME=$(terraform output -raw aks_cluster_name)
+
+az aks get-credentials --resource-group ${RESOURCE_GROUP} --name ${CLUSTER_NAME}
+
+TABLES_ADDRESS=$(kubectl get service openhouse-tables-service -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+TABLES_PORT=$(kubectl get service openhouse-tables-service -o jsonpath="{.spec.ports[*].port}")
+
+COMMAND="bin/spark-shell --packages org.apache.iceberg:iceberg-azure:1.5.0,org.apache.iceberg:iceberg-spark-runtime-3.1_2.12:1.2.0 \
+  --jars openhouse-spark-apps_2.12-*-all.jar,openhouse-spark-runtime_2.12-latest-all.jar  \
+  --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,com.linkedin.openhouse.spark.extensions.OpenhouseSparkSessionExtensions   \
+  --conf spark.sql.catalog.openhouse=org.apache.iceberg.spark.SparkCatalog   \
+  --conf spark.sql.catalog.openhouse.catalog-impl=com.linkedin.openhouse.spark.OpenHouseCatalog     \
+  --conf spark.sql.catalog.openhouse.metrics-reporter-impl=com.linkedin.openhouse.javaclient.OpenHouseMetricsReporter    \
+  --conf spark.sql.catalog.openhouse.uri=http://${TABLES_ADDRESS}:${TABLES_PORT}/  \
+  --conf spark.sql.catalog.openhouse.auth-token=\$(cat /var/config/\$(whoami).token) \
+  --conf spark.sql.catalog.openhouse.cluster=AKSCluster \
+  --conf spark.sql.catalog.openhouse.io-impl=org.apache.iceberg.azure.adlsv2.ADLSFileIO \
+  --conf spark.sql.catalog.openhouse.adls.auth.shared-key.account.name=${ACCOUNT_NAME} \
+  --conf spark.sql.catalog.openhouse.adls.auth.shared-key.account.key=${ACCOUNT_KEY}"
+
+echo $COMMAND > ./start-spark-shell.sh
+chmod +x ./start-spark-shell.sh
+
+docker cp ./start-spark-shell.sh local.spark-master:/opt/spark
+
+docker exec -it local.spark-master /bin/bash -c ./start-spark-shell.sh
+
+./start-spark-shell.sh
