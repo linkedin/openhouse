1c5b428de6a67148e2b0fb9dafe879a9bdef9dda
Kai McClennen <63942422+kmcclenn@users.noreply.github.com>
2024-08-05 11:34:59 -0700

    Added tests for ADLS Storage (#154)
    ## Summary
    
    <!--- HINT: Replace #nnn with corresponding Issue number, if you are
    fixing an existing issue -->
    
    [Issue](https://github.com/linkedin/openhouse/issues/#nnn)] Briefly
    discuss the summary of the changes made in this
    pull request in 2-3 lines.
    
    ## Changes
    
    - [ ] Client-facing API Changes
    - [ ] Internal API Changes
    - [ ] Bug Fixes
    - [ ] New Features
    - [ ] Performance Improvements
    - [ ] Code Style
    - [ ] Refactoring
    - [ ] Documentation
    - [x] Tests
    
    Added tests for the ADLS Storage and ADLS Storage Client classes.
    
    ## Testing Done
    <!--- Check any relevant boxes with "x" -->
    
    - [ ] Manually Tested on local docker setup. Please include commands
    ran, and their output.
    - [ ] Added new tests for the changes made.
    - [x] Updated existing tests to reflect the changes made.
    - [ ] No tests added or updated. Please explain why. If unsure, please
    feel free to ask for help.
    - [ ] Some other form of testing like staging or soak time in
    production. Please explain.
    
    All tests passed.
    
    # Additional Information
    
    - [ ] Breaking Changes
    - [ ] Deprecations
    - [ ] Large PR broken into smaller PRs, and PR plan linked in the
    description.
    
    For all the boxes checked, include additional details of the changes
    made in this pull request.

---
+++ services/tables/src/test/java/com/linkedin/openhouse/tables/mock/storage/adls/AdlsStorageTest.java
@@ -0,0 +1,81 @@
+package com.linkedin.openhouse.tables.mock.storage.adls;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.google.common.collect.ImmutableMap;
+import com.linkedin.openhouse.cluster.storage.StorageType;
+import com.linkedin.openhouse.cluster.storage.adls.AdlsStorage;
+import com.linkedin.openhouse.cluster.storage.adls.AdlsStorageClient;
+import com.linkedin.openhouse.cluster.storage.configs.StorageProperties;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class AdlsStorageTest {
+  @Autowired private AdlsStorage adlsStorage;
+
+  @MockBean private StorageProperties storageProperties;
+
+  @MockBean private AdlsStorageClient adlsStorageClient;
+
+  @Test
+  public void testAdlsStorageIsConfiguredWhenTypeIsProvided() {
+    when(storageProperties.getTypes())
+        .thenReturn(
+            ImmutableMap.of(
+                StorageType.ADLS.getValue(), new StorageProperties.StorageTypeProperties()));
+    assertTrue(adlsStorage.isConfigured());
+  }
+
+  @Test
+  public void testAdlsStorageTypeIsCorrect() {
+    assertEquals(StorageType.ADLS, adlsStorage.getType());
+  }
+
+  @Test
+  public void testAdlsStorageClientIsNotNull() {
+    when(storageProperties.getTypes())
+        .thenReturn(
+            ImmutableMap.of(
+                StorageType.ADLS.getValue(), new StorageProperties.StorageTypeProperties()));
+    assertNotNull(adlsStorageClient);
+  }
+
+  @Test
+  public void testAdlsStoragePropertiesReturned() {
+    Map<String, String> testMap = ImmutableMap.of("k1", "v1", "k2", "v2");
+    when(storageProperties.getTypes())
+        .thenReturn(
+            ImmutableMap.of(
+                StorageType.ADLS.getValue(),
+                new StorageProperties.StorageTypeProperties(
+                    "abfs://", "testcontainer@teststorage.dfs.core.windows.net", testMap)));
+    assertEquals(testMap, adlsStorage.getProperties());
+  }
+
+  @Test
+  public void testAllocateTableSpace() {
+    String databaseId = "db1";
+    String tableId = "table1";
+    String tableUUID = "uuid1";
+    String tableCreator = "creator1";
+    boolean skipProvisioning = false;
+    Map<String, String> testMap = ImmutableMap.of("k1", "v1", "k2", "v2");
+    when(storageProperties.getTypes())
+        .thenReturn(
+            ImmutableMap.of(
+                StorageType.ADLS.getValue(),
+                new StorageProperties.StorageTypeProperties(
+                    "testcontainer@teststorage.dfs.core.windows.net", "abfs://", testMap)));
+    when(adlsStorageClient.getEndpoint()).thenReturn("abfs://");
+    when(adlsStorageClient.getRootPrefix())
+        .thenReturn("testcontainer@teststorage.dfs.core.windows.net");
+    String expected = "abfs://testcontainer@teststorage.dfs.core.windows.net/db1/table1-uuid1";
+    assertEquals(
+        expected, adlsStorage.allocateTableLocation(databaseId, tableId, tableUUID, tableCreator));
+  }
+}
