be0bdc80bc3090f57b730bd2acf2b35161c37100
rohitkumar <rohitkumar2506@gmail.com>
2024-08-15 14:18:54 -0700

    minor change

--- apps/spark/src/test/java/com/linkedin/openhouse/jobs/util/DatabaseTableFilterTest.java
+++ apps/spark/src/test/java/com/linkedin/openhouse/jobs/util/DatabaseTableFilterTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DatabaseTableFilterTest {
-
   @Test
   void testAcceptAll() {
     DatabaseTableFilter filter = DatabaseTableFilter.of(".*", ".*");
