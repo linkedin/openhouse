b28a0d17b1bcecb81e733a8982c48ebaa7d6a86e
anjagruenheid <87397397+anjagruenheid@users.noreply.github.com>
2024-08-27 15:45:14 +0200

    Add entropy as an attribute to DLS.

--- libs/datalayout/src/main/java/com/linkedin/openhouse/datalayout/generator/OpenHouseDataLayoutStrategyGenerator.java
+++ libs/datalayout/src/main/java/com/linkedin/openhouse/datalayout/generator/OpenHouseDataLayoutStrategyGenerator.java
@@ -1,13 +1,19 @@
 package com.linkedin.openhouse.datalayout.generator;
 
+import com.linkedin.openhouse.datalayout.config.DataCompactionConfig;
+import com.linkedin.openhouse.datalayout.datasource.FileStat;
 import com.linkedin.openhouse.datalayout.datasource.TableFileStats;
 import com.linkedin.openhouse.datalayout.datasource.TablePartitionStats;
 import com.linkedin.openhouse.datalayout.strategy.DataLayoutStrategy;
-import com.linkedin.openhouse.datalayout.util.DataCompactionConfigUtil;
-import com.linkedin.openhouse.datalayout.util.TableFileStatsUtil;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import lombok.Builder;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.ReduceFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import scala.Tuple2;
 
 /**
@@ -19,8 +25,13 @@ public class OpenHouseDataLayoutStrategyGenerator implements DataLayoutStrategyG
   private static final long MB = 1024 * 1024;
   private static final long FILE_BLOCK_SIZE_BYTES = 256 * MB;
   private static final long FILE_BLOCK_MARGIN_BYTES = 10 * MB;
+  private static final long NUM_FILE_GROUPS_PER_COMMIT = 100;
+  private static final long MAX_NUM_COMMITS = 30;
+  private static final long MAX_BYTES_SIZE_RATIO = 10;
   private static final long REWRITE_BYTES_PER_SECOND = 2 * MB;
   private static final long EXECUTOR_MEMORY_GB = 2;
+  private static final int MAX_CONCURRENT_FILE_GROUP_REWRITES = 50;
+  private static final int REWRITE_PARALLELISM = 900; // number of Spark tasks to run in parallel
   private static final long TARGET_BYTES_SIZE = 2 * FILE_BLOCK_SIZE_BYTES - FILE_BLOCK_MARGIN_BYTES;
   private static final double COMPUTE_STARTUP_COST_GB_HR = 0.5;
   private final TableFileStats tableFileStats;
@@ -47,22 +58,64 @@ public class OpenHouseDataLayoutStrategyGenerator implements DataLayoutStrategyG
    * </ul>
    */
   private DataLayoutStrategy generateCompactionStrategy() {
-    Tuple2<Long, Integer> aggFileStats =
-        TableFileStatsUtil.getAggregatedFileStats(tableFileStats, TARGET_BYTES_SIZE);
-    long rewriteFileBytes = aggFileStats._1;
-    int rewriteFileCount = aggFileStats._2;
+    // Retrieve file sizes of all data files.
+    Dataset<Long> fileSizes =
+        tableFileStats.get().map((MapFunction<FileStat, Long>) FileStat::getSize, Encoders.LONG());
+
+    Tuple2<Long, Integer> fileStats =
+        fileSizes
+            .filter(
+                (FilterFunction<Long>)
+                    size ->
+                        size < TARGET_BYTES_SIZE * DataCompactionConfig.MIN_BYTE_SIZE_RATIO_DEFAULT)
+            .map(
+                (MapFunction<Long, Tuple2<Long, Integer>>) size -> new Tuple2<>(size, 1),
+                Encoders.tuple(Encoders.LONG(), Encoders.INT()))
+            .reduce(
+                (ReduceFunction<Tuple2<Long, Integer>>)
+                    (a, b) -> new Tuple2<>(a._1 + b._1, a._2 + b._2));
+    long rewriteFileBytes = fileStats._1;
+    int rewriteFileCount = fileStats._2;
+
+    DataCompactionConfig.DataCompactionConfigBuilder configBuilder = DataCompactionConfig.builder();
+
+    configBuilder.targetByteSize(TARGET_BYTES_SIZE);
+
+    long estimatedFileGroupsCount =
+        Math.max(
+            tablePartitionStats.get().count(),
+            rewriteFileBytes / DataCompactionConfig.MAX_FILE_GROUP_SIZE_BYTES_DEFAULT);
+
+    int maxCommitsCount =
+        (int)
+            Math.min(
+                MAX_NUM_COMMITS,
+                (estimatedFileGroupsCount + NUM_FILE_GROUPS_PER_COMMIT - 1)
+                    / NUM_FILE_GROUPS_PER_COMMIT);
+    configBuilder.partialProgressMaxCommits(maxCommitsCount);
+
+    int estimatedTasksPerFileGroupCount =
+        (int) (DataCompactionConfig.MAX_FILE_GROUP_SIZE_BYTES_DEFAULT / TARGET_BYTES_SIZE);
+
+    configBuilder.maxConcurrentFileGroupRewrites(
+        Math.min(
+            MAX_CONCURRENT_FILE_GROUP_REWRITES,
+            (estimatedTasksPerFileGroupCount + REWRITE_PARALLELISM - 1)
+                / estimatedTasksPerFileGroupCount));
+
+    // don't split large files
+    configBuilder.maxByteSizeRatio(MAX_BYTES_SIZE_RATIO);
 
     long reducedFileCount = estimateReducedFileCount(rewriteFileBytes, rewriteFileCount);
     double computeGbHr = estimateComputeGbHr(rewriteFileBytes);
     // computeGbHr >= COMPUTE_STARTUP_COST_GB_HR
     double reducedFileCountPerComputeGbHr = reducedFileCount / computeGbHr;
     return DataLayoutStrategy.builder()
-        .config(
-            DataCompactionConfigUtil.createDefaultCompactionConfig(
-                TARGET_BYTES_SIZE, tablePartitionStats.get().count(), rewriteFileBytes))
+        .config(configBuilder.build())
         .cost(computeGbHr)
         .gain(reducedFileCount)
         .score(reducedFileCountPerComputeGbHr)
+        .entropy(computeEntropy(fileSizes))
         .build();
   }
 
@@ -76,5 +129,15 @@ public class OpenHouseDataLayoutStrategyGenerator implements DataLayoutStrategyG
     double rewriteSeconds = rewriteBytes * 1.0 / REWRITE_BYTES_PER_SECOND;
     double rewriteHours = rewriteSeconds / 3600.0;
     return rewriteHours * EXECUTOR_MEMORY_GB + COMPUTE_STARTUP_COST_GB_HR;
+  }
+
+  private double computeEntropy(Dataset<Long> fileSizes) {
+    double mse = 0.0;
+    Iterator<Long> itr = fileSizes.toLocalIterator();
+    while (itr.hasNext()) {
+      mse += Math.pow(TARGET_BYTES_SIZE - itr.next(), 2);
+    }
+    // Normalize.
+    return mse / fileSizes.count();
   }
 }
