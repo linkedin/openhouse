25c61ade4dd57954306db21db4f0649b4d1a6011
Lei Sun <lesun@lesun-mn1.linkedin.biz>
2024-09-03 15:50:33 -0700

    fix unit test in text-fixture module

--- tables-test-fixtures/src/main/java/com/linkedin/openhouse/tablestest/SpringH2TestApplication.java
+++ tables-test-fixtures/src/main/java/com/linkedin/openhouse/tablestest/SpringH2TestApplication.java
@@ -18,14 +18,14 @@ import org.springframework.context.annotation.Primary;
     basePackages = {
       "com.linkedin.openhouse.tables.api",
       "com.linkedin.openhouse.tables.audit",
-      "com.linkedin.openhouse.tables.toggle",
       "com.linkedin.openhouse.tables.authorization",
       "com.linkedin.openhouse.tables.dto.mapper",
       "com.linkedin.openhouse.tables.utils",
       "com.linkedin.openhouse.tables.controller",
       "com.linkedin.openhouse.tables.services",
       "com.linkedin.openhouse.tables.config",
-      "com.linkedin.openhouse.tables.toggle.repository",
+      "com.linkedin.openhouse.internal.catalog.toggle.repository",
+      "com.linkedin.openhouse.internal.catalog.toggle",
       "com.linkedin.openhouse.internal.catalog",
       "com.linkedin.openhouse.cluster.configs",
       "com.linkedin.openhouse.cluster.storage",
@@ -36,7 +36,8 @@ import org.springframework.context.annotation.Primary;
 @EntityScan(
     basePackages = {
       "com.linkedin.openhouse.tables.model",
-      "com.linkedin.openhouse.internal.catalog.model"
+      "com.linkedin.openhouse.internal.catalog.model",
+      "com.linkedin.openhouse.internal.catalog.toggle.model"
     })
 @EnableAutoConfiguration(
     exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
