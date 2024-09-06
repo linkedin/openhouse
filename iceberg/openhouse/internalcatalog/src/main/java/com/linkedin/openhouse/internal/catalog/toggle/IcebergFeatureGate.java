0a0a179b9394fae3182b23d69d92231e8b3775a4
Lei Sun <lesun@linkedin.com>
2024-09-06 11:14:01 -0700

    Add unit tests for featureGateAspect

--- iceberg/openhouse/internalcatalog/src/main/java/com/linkedin/openhouse/internal/catalog/toggle/IcebergFeatureGate.java
+++ iceberg/openhouse/internalcatalog/src/main/java/com/linkedin/openhouse/internal/catalog/toggle/IcebergFeatureGate.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** Used to gate the feature of the corresponding method where this annotation is applied. */
+/**
+ * Used to gate the feature of the corresponding method where this annotation is applied, on the
+ * granularity of Iceberg table identified by {@link org.apache.iceberg.catalog.TableIdentifier}
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface IcebergFeatureGate {
--- services/tables/src/main/java/com/linkedin/openhouse/tables/toggle/FeatureGateAspect.java
+++ services/tables/src/main/java/com/linkedin/openhouse/tables/toggle/FeatureGateAspect.java
@@ -1,5 +1,6 @@
 package com.linkedin.openhouse.tables.toggle;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.linkedin.openhouse.internal.catalog.toggle.IcebergFeatureGate;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.iceberg.catalog.TableIdentifier;
@@ -9,6 +10,10 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * To use {@link TableFeatureToggle} one can add implementation of feature-toggle checking in this
+ * class with different types of advises injected (e.g. {@link Before}).
+ */
 @Aspect
 @Component
 @Slf4j
@@ -18,12 +23,29 @@ public class FeatureGateAspect {
 
   @Before("@annotation(featureGate)")
   public void checkIcebergFeatureFlag(JoinPoint joinPoint, IcebergFeatureGate featureGate) {
-    TableIdentifier tableIdentifier = (TableIdentifier) joinPoint.getArgs()[0];
+    TableIdentifier tableIdentifier = obtainIdFromAnnotationArgs(joinPoint);
 
     if (tableFeatureToggle.isFeatureActivated(
         tableIdentifier.namespace().toString(), tableIdentifier.name(), featureGate.value())) {
       throw new ResourceGatedByToggledOnFeatureException(
           featureGate.value(), tableIdentifier.namespace().toString(), tableIdentifier.name());
     }
+  }
+
+  /** Obtain {@link TableIdentifier} from the list of arguments. */
+  @VisibleForTesting
+  TableIdentifier obtainIdFromAnnotationArgs(JoinPoint joinPoint) {
+    for (Object arg : joinPoint.getArgs()) {
+      if (arg instanceof TableIdentifier) {
+        return (TableIdentifier) arg;
+      }
+    }
+
+    // TODO:
+    // * Enhance exception handling together with ResourceGatedByToggledOnFeatureException using
+    // exception-handler,
+    // right now it just pops as internal server error *
+    throw new RuntimeException(
+        "(Server error)Cannot find TableIdentifier from the method annotated by IcebergFeatureGate");
   }
 }
