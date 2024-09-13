49dd6013ae1d1bfbe3d72e73983972d7c4712eed
Vikram Bohra <bohravikram@gmail.com>
2024-09-13 08:50:19 -0700

    Lazily initialize storage selectors in storageselectorconfig

--- cluster/storage/src/main/java/com/linkedin/openhouse/cluster/storage/selector/StorageSelectorConfig.java
+++ cluster/storage/src/main/java/com/linkedin/openhouse/cluster/storage/selector/StorageSelectorConfig.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 
 /**
@@ -21,7 +22,7 @@ public class StorageSelectorConfig {
 
   @Autowired StorageProperties storageProperties;
 
-  @Autowired List<StorageSelector> storageSelectors;
+  @Lazy @Autowired List<StorageSelector> storageSelectors;
 
   @Autowired DefaultStorageSelector defaultStorageSelector;
 
