af70149c6662e4cb0ad4342e79147820fc9a4b1a
Levi Jiang <lejiang@linkedin.com>
2024-11-07 13:31:19 -0800

    Build succeeded after code dedup

--- integrations/java-iceberg-1.5/openhouse-java-runtime/src/main/java/com/linkedin/openhouse/javaclient/audit/OpenHouseReportHandler.java
+++
@@ -1,9 +0,0 @@
-package com.linkedin.openhouse.javaclient.audit;
-
-/**
- * interface to handle Iceberg audit events. mandates handling commitEvent and ScanEvent for Iceberg
- * Audit events
- */
-public interface OpenHouseReportHandler<MetricsReport> {
-  void audit(MetricsReport metricsReport);
-}
--- integrations/spark-3.5/openhouse-spark-runtime/src/main/scala/com/linkedin/openhouse/spark/sql/catalyst/enums/GrantableResourceTypes.scala
+++
@@ -1,6 +0,0 @@
-package com.linkedin.openhouse.spark.sql.catalyst.enums
-
-private[sql] object GrantableResourceTypes extends Enumeration {
-  type GrantableResourceType = Value
-  val TABLE, DATABASE = Value
-}
