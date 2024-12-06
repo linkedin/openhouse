14506b62fbfb1af9cc89f707b5d9ee2441e74ac6
Sushant Raikar <sraikar@linkedin.com>
2024-12-06 10:35:01 -0800

    Consider WebClientRequestException as a CSUE (#266)
    ## Summary
    Network cutoff between Client and Server surface as
    WebClientRequestException.
    
    Following situation can arise:
    - Client makes a docommit() call that succeeds
    - Server responds success
    - Network cuts off
    - Client gets WebClientRequestException
    - Client deletes snapshots and added data
    
    The state of the table can end up corrupted.
    
    ## Details
    ### When is WebClientRequestException Thrown
    WebClientRequestException is thrown only **if something goes wrong
    before response (even if failure) is completely received** see the [code
    block
    here](https://github.com/spring-projects/spring-framework/blob/5e939ccb4ba26243e765dd37f37eda90f90292d8/spring-webflux/src/main/java/org/springframework/web/reactive/function/client/ExchangeFunctions.java#L106):
    ```
    return this.connector
    		.connect(httpMethod, url, httpRequest -> clientRequest.writeTo(httpRequest, this.strategies))
    		.doOnRequest(n -> logRequest(clientRequest))
    		.doOnCancel(() -> logger.debug(clientRequest.logPrefix() + "Cancel signal (to close connection)"))
    		.onErrorResume(WebClientUtils.WRAP_EXCEPTION_PREDICATE, t -> wrapException(t, clientRequest))     ====> this code throws WebClientRequestException.
    		.map(httpResponse -> {
    			String logPrefix = getLogPrefix(clientRequest, httpResponse);
    			logResponse(httpResponse, logPrefix);
    			return new DefaultClientResponse(
    					httpResponse, this.strategies, logPrefix, httpMethod.name() + " " + url,
    					() -> createRequest(clientRequest));
    		});
    ```
    ^ This means the request could've been processed.
    
    ## Changes
    
    - [ ] Client-facing API Changes
    - [ ] Internal API Changes
    - [x] Bug Fixes
    - [ ] New Features
    - [ ] Performance Improvements
    - [ ] Code Style
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Tests
    
    For all the boxes checked, please include additional details of the
    changes made in this pull request.
    
    ## Testing Done
    <!--- Check any relevant boxes with "x" -->
    
    - [ ] Manually Tested on local docker setup. Please include commands
    ran, and their output.
    - [ ] Added new tests for the changes made.
    - [X] Updated existing tests to reflect the changes made.
    - [ ] No tests added or updated. Please explain why. If unsure, please
    feel free to ask for help.
    - [ ] Some other form of testing like staging or soak time in
    production. Please explain.
    
    For all the boxes checked, include a detailed description of the testing
    done for the changes made in this pull request.
    
    # Additional Information
    
    - [ ] Breaking Changes
    - [ ] Deprecations
    - [ ] Large PR broken into smaller PRs, and PR plan linked in the
    description.
    
    For all the boxes checked, include additional details of the changes
    made in this pull request.

--- integrations/java/iceberg-1.2/openhouse-java-runtime/src/main/java/com/linkedin/openhouse/javaclient/OpenHouseTableOperations.java
+++ integrations/java/iceberg-1.2/openhouse-java-runtime/src/main/java/com/linkedin/openhouse/javaclient/OpenHouseTableOperations.java
@@ -299,17 +299,21 @@ public class OpenHouseTableOperations extends BaseMetastoreTableOperations {
       return Mono.error(
           new BadRequestException(
               casted, casted.getStatusCode().value() + " , " + casted.getResponseBodyAsString()));
-    } else if (e instanceof WebClientResponseException) {
+    } else if (e instanceof WebClientResponseException
+        && ((WebClientResponseException) e).getStatusCode().is4xxClientError()) {
       return Mono.error(new WebClientResponseWithMessageException((WebClientResponseException) e));
-    } else if (e instanceof WebClientRequestException) {
-      return Mono.error(new WebClientRequestWithMessageException((WebClientRequestException) e));
     } else {
       /**
        * This serves as a catch-all for any unexpected exceptions that could occur during doCommit,
        * (i.e) exceptions that are not WebClientResponseException. This is a conservative approach
        * to skip any unexpected cleanup that could occur when a commit aborts at the caller, thus
-       * avoiding any potential data loss.
+       * avoiding any potential data loss. {@link WebClientRequestException} is caught here.
        */
+      log.error(
+          String.format(
+              "Unexpected exception occurred during doCommit: %s, with stacktrace: ",
+              e.getClass().getSimpleName()),
+          e);
       return Mono.error(new CommitStateUnknownException(e));
     }
   }
