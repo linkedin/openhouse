0ad93ba28aa36b2bff767e5f0fe2380cfb208798
Sushant Raikar <sraikar@linkedin.com>
2024-12-17 11:51:32 -0800

    Revert "Skip field-id re-assignment during table creation (#247)" (#272)
    This reverts commit 6d69b6cd824ace8a27c18d181c3f189c972aec4c.
    
    ## Summary
    
    <!--- HINT: Replace #nnn with corresponding Issue number, if you are
    fixing an existing issue -->
    
    [Issue](https://github.com/linkedin/openhouse/issues/#nnn)] Briefly
    discuss the summary of the changes made in this
    pull request in 2-3 lines.
    
    ## Changes
    
    - [ ] Client-facing API Changes
    - [ ] Internal API Changes
    - [ ] Bug Fixes
    - [X] New Features
    - [ ] Performance Improvements
    - [ ] Code Style
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Tests
    
    For all the boxes checked, please include additional details of the
    changes made in this pull request.
    
    ## Testing Done
    <!--- Check any relevant boxes with "x" -->
    
    - [ ] Manually Tested on local docker setup. Please include commands
    ran, and their output.
    - [ ] Added new tests for the changes made.
    - [ ] Updated existing tests to reflect the changes made.
    - [ ] No tests added or updated. Please explain why. If unsure, please
    feel free to ask for help.
    - [X] Some other form of testing like staging or soak time in
    production. Please explain.
    
    For all the boxes checked, include a detailed description of the testing
    done for the changes made in this pull request.
    
    # Additional Information
    
    - [ ] Breaking Changes
    - [ ] Deprecations
    - [ ] Large PR broken into smaller PRs, and PR plan linked in the
    description.
    
    For all the boxes checked, include additional details of the changes
    made in this pull request.

--- iceberg/openhouse/internalcatalog/src/main/java/com/linkedin/openhouse/internal/catalog/CatalogConstants.java
+++ iceberg/openhouse/internalcatalog/src/main/java/com/linkedin/openhouse/internal/catalog/CatalogConstants.java
@@ -17,8 +17,6 @@ public final class CatalogConstants {
 
   static final String FEATURE_TOGGLE_STOP_CREATE = "stop_create";
 
-  static final String CLIENT_TABLE_SCHEMA = "client.table.schema";
-
   private CatalogConstants() {
     // Noop
   }
