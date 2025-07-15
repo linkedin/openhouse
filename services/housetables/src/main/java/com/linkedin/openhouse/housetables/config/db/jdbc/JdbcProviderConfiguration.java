b0976ab95df14459be457d9a0f31aed08d60113b
cbb330 <chrisbush747@gmail.com>
2025-07-15 14:18:48 -0700

    bumping hikaricp resources and updating configuration

--- services/housetables/src/main/java/com/linkedin/openhouse/housetables/config/db/jdbc/JdbcProviderConfiguration.java
+++ services/housetables/src/main/java/com/linkedin/openhouse/housetables/config/db/jdbc/JdbcProviderConfiguration.java
@@ -2,15 +2,14 @@ package com.linkedin.openhouse.housetables.config.db.jdbc;
 
 import com.linkedin.openhouse.cluster.configs.ClusterProperties;
 import com.linkedin.openhouse.housetables.config.db.DatabaseConfiguration;
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Configure JDBC sources such as h2, mysql, postgres
@@ -26,18 +25,17 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class JdbcProviderConfiguration {
 
-  @Autowired private ClusterProperties clusterProperties;
+  private final ClusterProperties clusterProperties;
 
-  @Bean
-  @ConfigurationProperties("spring.datasource")
-  public DataSourceProperties dataSourceProperties() {
-    return new DataSourceProperties();
+  public JdbcProviderConfiguration(ClusterProperties clusterProperties) {
+    this.clusterProperties = clusterProperties;
   }
 
   @Bean
-  @ConfigurationProperties("spring.datasource.hikari")
-  public HikariConfig hikariConfig() {
-    return new HikariConfig();
+  @Primary
+  @ConfigurationProperties("spring.datasource")
+  public DataSourceProperties dataSourceProperties() {
+    return new DataSourceProperties();
   }
 
   /**
@@ -48,22 +46,26 @@ public class JdbcProviderConfiguration {
   private static final String H2_DEFAULT_URL = "jdbc:h2:mem:htsdb;MODE=MySQL;DB_CLOSE_DELAY=-1";
 
   @Bean
-  public DataSource provideJdbcDataSource() {
+  @Primary
+  @ConfigurationProperties("spring.datasource.hikari")
+  public DataSource dataSource() {
     DatabaseConfiguration.SupportedDbTypes dbType =
         DatabaseConfiguration.SupportedDbTypes.valueOf(
             clusterProperties.getClusterHouseTablesDatabaseType());
 
-    log.info(String.format("Using %s database for HouseTables service", dbType));
+    log.info("Using {} database for HouseTables service", dbType);
 
-    HikariConfig config = hikariConfig();
-    //  if storage type is Iceberg, use H2 as the default jdbc database
-    config.setJdbcUrl(
-        dbType == DatabaseConfiguration.SupportedDbTypes.ICEBERG
-            ? H2_DEFAULT_URL
-            : clusterProperties.getClusterHouseTablesDatabaseUrl());
-    config.setUsername(clusterProperties.getClusterHouseTablesDatabaseUsername());
-    config.setPassword(clusterProperties.getClusterHouseTablesDatabasePassword());
+    DataSourceProperties properties = dataSourceProperties();
 
-    return new HikariDataSource(config);
+    // Set database-specific properties
+    if (dbType == DatabaseConfiguration.SupportedDbTypes.ICEBERG) {
+      properties.setUrl(H2_DEFAULT_URL);
+    } else {
+      properties.setUrl(clusterProperties.getClusterHouseTablesDatabaseUrl());
+      properties.setUsername(clusterProperties.getClusterHouseTablesDatabaseUsername());
+      properties.setPassword(clusterProperties.getClusterHouseTablesDatabasePassword());
+    }
+
+    return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
   }
 }
