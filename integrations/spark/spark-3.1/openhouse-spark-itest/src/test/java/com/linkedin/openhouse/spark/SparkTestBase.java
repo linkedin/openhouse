9b9c7e7b625216b78591eed44dee1b075fc63dd4
Will-Lo <wlo@apache.org>
2025-07-28 15:17:48 -0400

    Fix build

--- integrations/spark/spark-3.1/openhouse-spark-itest/src/test/java/com/linkedin/openhouse/spark/SparkTestBase.java
+++ integrations/spark/spark-3.1/openhouse-spark-itest/src/test/java/com/linkedin/openhouse/spark/SparkTestBase.java
@@ -39,9 +39,7 @@ public class SparkTestBase
                   "spark.sql.extensions",
                   ("org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,"
                       + "com.linkedin.openhouse.spark.extensions.OpenhouseSparkSessionExtensions"))
-              .config(
-                  "spark.sql.catalog.openhouse",
-                  "com.linkedin.openhouse.spark.OpenHouseSparkCatalog")
+              .config("spark.sql.catalog.openhouse", "org.apache.iceberg.spark.SparkCatalog")
               .config(
                   "spark.sql.catalog.openhouse.catalog-impl",
                   "com.linkedin.openhouse.spark.OpenHouseCatalog")
