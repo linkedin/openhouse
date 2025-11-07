27b95e07bf4ea307f812d9b8f25aec21dbbcc9c8
srawat <srawat@linkedin.com>
2025-11-07 15:33:30 +0530

    Refactor BaseEventModels to simplify BaseCommitEvent structure; remove inheritance from BaseDataset

--- services/common/src/main/java/com/linkedin/openhouse/common/stats/model/BaseEventModels.java
+++ services/common/src/main/java/com/linkedin/openhouse/common/stats/model/BaseEventModels.java
@@ -2,7 +2,6 @@ package com.linkedin.openhouse.common.stats.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
@@ -19,7 +18,7 @@ public final class BaseEventModels {
   @SuperBuilder
   @NoArgsConstructor
   @AllArgsConstructor
-  public abstract static class BaseDataset {
+  public static class BaseDataset {
 
     /** Name of the database for the dataset */
     @NonNull private String databaseName;
@@ -31,13 +30,15 @@ public final class BaseEventModels {
     @NonNull private String clusterName;
   }
 
-  /** Base class for commit events. */
+  /** Base class for commit events that contains dataset information. */
   @Data
   @SuperBuilder
   @NoArgsConstructor
   @AllArgsConstructor
-  @EqualsAndHashCode(callSuper = true)
-  public abstract static class BaseCommitEvent extends BaseDataset {
+  public abstract static class BaseCommitEvent {
+
+    /** Dataset information for this commit event */
+    @NonNull private BaseDataset dataset;
 
     /** Unique identifier for each commit event */
     @NonNull private String commitId;
