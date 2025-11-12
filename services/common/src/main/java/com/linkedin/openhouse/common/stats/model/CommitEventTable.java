5bc20cee482ea04a35ebf780f554f89825cc7a09
srawat <srawat@linkedin.com>
2025-11-12 19:20:41 +0530

    rebase

--- services/common/src/main/java/com/linkedin/openhouse/common/stats/model/CommitEventTable.java
+++ services/common/src/main/java/com/linkedin/openhouse/common/stats/model/CommitEventTable.java
@@ -10,8 +10,8 @@ import lombok.experimental.SuperBuilder;
 /**
  * Data model for openhouseTableCommitEvents table.
  *
- * <p>Stores commit-level metadata for dataset changes. Each record represents a commit on table
- * and can be linked to multiple partition events via commitId.
+ * <p>Stores commit-level metadata for dataset changes. Each record represents a commit on table and
+ * can be linked to multiple partition events via commitId.
  *
  * <p><b>Cardinality</b>: One commit event can have N partition events. See {@link
  * CommitEventTablePartitions} for partition-level details.
