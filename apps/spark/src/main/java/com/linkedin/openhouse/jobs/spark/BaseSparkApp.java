a3781dbc24aeb38478ec272f4651532214cb316b
William Lo <wlo@apache.org>
2025-11-26 17:58:28 -0500

    Refactor common utilities of maintenance jobs into a base clas (#401)
    ## Summary
    
    <!--- HINT: Replace #nnn with corresponding Issue number, if you are
    fixing an existing issue -->
    
    [Issue](https://github.com/linkedin/openhouse/issues/#nnn)] Briefly
    discuss the summary of the changes made in this
    pull request in 2-3 lines.
    
    To generalize maintenance jobs to not be as tied to Spark
    implementation, other job types will need access to the same helper
    functions for `onStarted`, `onFinished`, sending heartbeats, setting up
    metrics, creating the CLI
    
    For now to avoid large scale changes, still keep this within the same
    module but perform the refactor.
    In future PRs will take this class out of the Spark module into a common
    module.
    
    
    ## Changes
    
    - [ ] Client-facing API Changes
    - [ ] Internal API Changes
    - [ ] Bug Fixes
    - [ ] New Features
    - [ ] Performance Improvements
    - [ ] Code Style
    - [x] Refactoring
    - [ ] Documentation
    - [ ] Tests
    
    For all the boxes checked, please include additional details of the
    changes made in this pull request.
    
    ## Testing Done
    <!--- Check any relevant boxes with "x" -->
    
    - [ ] Manually Tested on local docker setup. Please include commands
    ran, and their output.
    - [ ] Added new tests for the changes made.
    - [ ] Updated existing tests to reflect the changes made.
    - [ ] No tests added or updated. Please explain why. If unsure, please
    feel free to ask for help.
    - [ ] Some other form of testing like staging or soak time in
    production. Please explain.
    
    For all the boxes checked, include a detailed description of the testing
    done for the changes made in this pull request.
    
    # Additional Information
    
    - [ ] Breaking Changes
    - [ ] Deprecations
    - [ ] Large PR broken into smaller PRs, and PR plan linked in the
    description.
    
    For all the boxes checked, include additional details of the changes
    made in this pull request.

--- apps/spark/src/main/java/com/linkedin/openhouse/jobs/spark/BaseSparkApp.java
+++ apps/spark/src/main/java/com/linkedin/openhouse/jobs/spark/BaseSparkApp.java
@@ -1,29 +1,10 @@
 package com.linkedin.openhouse.jobs.spark;
 
-import com.linkedin.openhouse.client.ssl.HousetablesApiClientFactory;
-import com.linkedin.openhouse.common.JobState;
 import com.linkedin.openhouse.common.metrics.OtelEmitter;
-import com.linkedin.openhouse.housetables.client.api.JobApi;
-import com.linkedin.openhouse.housetables.client.invoker.ApiClient;
 import com.linkedin.openhouse.jobs.spark.state.StateManager;
-import com.linkedin.openhouse.jobs.util.AppConstants;
-import com.linkedin.openhouse.jobs.util.RetryUtil;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
-import java.net.MalformedURLException;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import javax.net.ssl.SSLException;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.spark.sql.SparkSession;
 
 /**
@@ -31,18 +12,13 @@ import org.apache.spark.sql.SparkSession;
  * creation, args parsing.
  */
 @Slf4j
-public abstract class BaseSparkApp {
-  private static final long HEARTBEAT_INTERVAL_SECONDS_DEFAULT = 300;
+public abstract class BaseSparkApp extends BaseApp {
   protected static final String METRICS_SCOPE = BaseSparkApp.class.getName();
-  protected final String jobId;
-  protected final StateManager stateManager;
-  private final long heartbeatIntervalSeconds;
-  protected final OtelEmitter otelEmitter;
   private final ScheduledExecutorService scheduledExecutorService =
       Executors.newSingleThreadScheduledExecutor();
 
   protected BaseSparkApp(String jobId, StateManager stateManager, OtelEmitter otelEmitter) {
-    this(jobId, stateManager, HEARTBEAT_INTERVAL_SECONDS_DEFAULT, otelEmitter);
+    super(jobId, stateManager, HEARTBEAT_INTERVAL_SECONDS_DEFAULT, otelEmitter);
   }
 
   protected BaseSparkApp(
@@ -50,10 +26,7 @@ public abstract class BaseSparkApp {
       StateManager stateManager,
       long heartbeatIntervalSeconds,
       OtelEmitter otelEmitter) {
-    this.jobId = jobId;
-    this.stateManager = stateManager;
-    this.heartbeatIntervalSeconds = heartbeatIntervalSeconds;
-    this.otelEmitter = otelEmitter;
+    super(jobId, stateManager, heartbeatIntervalSeconds, otelEmitter);
   }
 
   public void run() {
@@ -83,86 +56,4 @@ public abstract class BaseSparkApp {
   }
 
   protected abstract void runInner(Operations ops) throws Exception;
-
-  protected static CommandLine createCommandLine(String[] args, List<Option> extraOptions) {
-    Options options = new Options();
-    options.addOption(new Option("i", "jobId", true, "Job id"));
-    options.addOption(new Option("d", "storageURL", true, "HTS endpoint URL"));
-    for (Option option : extraOptions) {
-      options.addOption(option);
-    }
-    CommandLineParser parser = new BasicParser();
-    try {
-      return parser.parse(options, args);
-    } catch (ParseException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  protected static StateManager createStateManager(CommandLine cmdLine, OtelEmitter otelEmitter) {
-    return new StateManager(
-        RetryUtil.getJobsStateApiRetryTemplate(),
-        createJobApiClient(cmdLine.getOptionValue("storageURL"), otelEmitter));
-  }
-
-  protected static String getJobId(CommandLine cmdLine) {
-    return cmdLine.getOptionValue("jobId");
-  }
-
-  private static JobApi createJobApiClient(String basePath, OtelEmitter otelEmitter) {
-    ApiClient client = null;
-    try {
-      client = HousetablesApiClientFactory.getInstance().createApiClient(basePath, null, null);
-    } catch (MalformedURLException | SSLException e) {
-      log.error("Jobs Api client creation failed: Failure while initializing ApiClient", e);
-      otelEmitter.count(
-          METRICS_SCOPE,
-          AppConstants.JOBS_CLIENT_INITIALIZATION_ERROR,
-          1,
-          Attributes.of(
-              AttributeKey.stringKey(AppConstants.SERVICE_NAME), AppConstants.SERVICE_HOUSETABLES));
-      throw new RuntimeException(e);
-    }
-    return new JobApi(client);
-  }
-
-  private void onStarted() {
-    log.info("onStarted");
-    scheduledExecutorService.scheduleAtFixedRate(
-        new HeartBeatTask(jobId, stateManager), 0, heartbeatIntervalSeconds, TimeUnit.SECONDS);
-    stateManager.updateStartTime(jobId);
-    stateManager.updateState(jobId, JobState.RUNNING);
-  }
-
-  private void onFinished(boolean success) {
-    log.info("onFinished");
-    stateManager.updateFinishTime(jobId);
-    if (success) {
-      stateManager.updateState(jobId, JobState.SUCCEEDED);
-      otelEmitter.count(
-          METRICS_SCOPE,
-          AppConstants.RUN_COUNT,
-          1,
-          Attributes.of(AttributeKey.stringKey(AppConstants.STATUS), AppConstants.SUCCESS));
-    } else {
-      stateManager.updateState(jobId, JobState.FAILED);
-      otelEmitter.count(
-          METRICS_SCOPE,
-          AppConstants.RUN_COUNT,
-          1,
-          Attributes.of(AttributeKey.stringKey(AppConstants.STATUS), AppConstants.FAIL));
-    }
-    scheduledExecutorService.shutdown();
-  }
-
-  @AllArgsConstructor
-  static class HeartBeatTask implements Runnable {
-    private String jobId;
-    private StateManager jobStateManager;
-
-    @Override
-    public void run() {
-      jobStateManager.sendHeartbeat(jobId);
-    }
-  }
 }
